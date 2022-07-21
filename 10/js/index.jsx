d234eb263c3ab16debf1e1800e7750e5d42c3c34
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-07-21 16:33:01 +0300

    lesson_ten

---
+++ 10/js/index.jsx
@@ -0,0 +1,44 @@
+var lastNameId;
+var lastNameEl;
+var firstNameId;
+var firstNameEl;
+var addressId;
+var addressEl;
+var sitiesId;
+var sitiesEl;
+var hobbiesId;
+var hobbiesEl;
+var avatarWrapperId;
+var avatarWrapperEl;
+var avatarId;
+var avatarEl;
+
+firstNameId = 'first-name';
+firstNameEl = document.getElementById(firstNameId);
+lastNameId = 'last-name';
+lastNameEl = document.getElementById(lastNameId);
+addressId = 'address';
+addressEl = document.getElementById(addressId);
+sitiesId = 'sities';
+sitiesEl = document.getElementById(sitiesId);
+hobbiesId = 'hobbies';
+hobbiesEl = document.getElementById(hobbiesId);
+avatarId = 'avatar';
+avatarEl = document.getElementById(avatarId);
+
+// window.alert(firstNameEl.value);
+// window.alert(lastNameEl.className);
+// window.alert(addressEl.value);
+// window.alert(sitiesEl.value);
+// window.alert(hobbiesEl.value);
+// window.alert(avatarEl.alt);
+
+firstNameEl.value = 'Pina';
+// lastNameEl.value='Odanova';
+lastNameEl.setAttribute = ('value', 'Koio');
+//    window.alert(lastNameEl.className);
+//    lastNameEl.className = 'error-input';
+//    avatarEl.title = 'Kartinka';
+    hobbiesEl.value = 'I am have not interests';
+    sitiesEl.value = 'Paris';
+    // avatarWrapperEl.innerHTML = 'dfvdevbdrb';
