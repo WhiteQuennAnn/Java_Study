f6d687b6ebf463b67b86c6d4c79f26ba55d2480a
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-07-22 08:30:43 +0300

    lesson_thirteen

---
+++ 13/js/index1.jsx
@@ -0,0 +1,40 @@
+// function firstNameLog () {
+
+// var firstNameId = 'first-name';
+// var firstNameEl = document.getElementById(firstNameId);
+// console.log (firstNameEl.value);
+// }
+// firstNameLog ();
+// firstNameLog ();
+
+// function lastNameLog () {
+
+//     var lastNameId = 'last-name';
+//     var lastNameEl = document.getElementById(lastNameId);
+//     console.log (lastNameEl.value);
+//     }
+   
+//     lastNameLog ();
+    
+
+    function logValue (id) {
+        var el = document.getElementById(id)
+        console.log (el.value);
+    }
+
+    
+    function getValue (id) {
+        var el = document.getElementById(id)
+        return el.value;
+    }
+    var firstNameId = 'first-name';
+    var lastNameId = 'last-name';
+
+    logValue (firstNameId);
+    logValue (lastNameId);
+
+    var value = getValue (lastNameId);
+    window.alert (value);
+
+
+
