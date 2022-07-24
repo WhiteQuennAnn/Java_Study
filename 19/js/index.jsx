9dcfbaaac6f3d9e06e928ce84587de32d8a8ad07
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-07-24 22:10:14 +0300

    lesson_nineteen

---
+++ 19/js/index.jsx
@@ -0,0 +1,45 @@
+const { array } = require("prop-types");
+
+var buttonPlus = document.getElementById('buttonPlus');
+var buttonMinus = document.getElementById('buttonMinus');
+var buttonMultiply = document.getElementById('buttonMultiply');
+var buttonDevide = document.getElementById('buttonDevide');
+
+var input1 = document.getElementById('number1');
+var input2 = document.getElementById('number2');
+
+function makeOperation(operationCode) {
+    var number1 = Number(input1.value);
+    var number2 = Number(input1.value);
+
+    if (operationCode === "+") {
+        var result = number1() + number2();
+    } else if (operationCode === "-") {
+        var result = number1() - number2();
+    } (operationCode === "*") {
+        var result = number1() * number2();
+    } (operationCode === "/") {
+        var result = number1() / number2();
+    } else {
+        window.alert('unknown operation')
+    };
+    window.alert(result);
+}
+
+
+function onOperationButtonClick(eventObject) {
+    var clickedElement = eventObject.currentTarget;
+    var operation = clickedElement.innerHTML;
+    makeOperation(operation)
+}
+
+
+
+var operationButtons [buttonPlus, buttonMinus, buttonMultiply, buttonDevide];
+var i;
+for (i = 0; i < operationButtons.length; i++) {
+    var button = operationButtons [i]
+    button.addEventListener('click', onOperationButtonClick);
+}
+addCommonEventListener (i);
+
